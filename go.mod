ce230ebbbf3b8b3465a9281108d6e53ef18a4be9
Gyuri Horak <dyuri@horak.hu>
2023-12-11 17:19:35 +0100

    zerolog, hex support

--- go.mod
+++ go.mod
@@ -2,6 +2,13 @@ module github.com/dyuri/go-udpproxy
 
 go 1.21.4
 
-require github.com/jessevdk/go-flags v1.5.0
+require (
+	github.com/jessevdk/go-flags v1.5.0
+	github.com/rs/zerolog v1.31.0
+)
 
-require golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"log"
 	"net"
 	"os"
 	"strings"
 
 	"github.com/jessevdk/go-flags"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 var opts struct {
@@ -17,66 +16,64 @@ var opts struct {
 	Buffer int    `short:"b" long:"buffer" default:"10240" description:"Buffer size for packets"`
 	Dump   bool   `short:"d" long:"dump" description:"Dump packets to stdout"`
 	Json   bool   `short:"j" long:"json" description:"Output JSON"`
+	Hex    bool   `short:"x" long:"hex" description:"Output hex"`
 }
 
 func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
 	_, err := flags.Parse(&opts)
 	if err != nil {
-		log.Printf("Error parsing flags: %s", err)
-		os.Exit(1)
+		log.Fatal().Err(err).Msg("Error parsing flags")
 	}
 
 	sourceAddr, err := net.ResolveUDPAddr("udp", opts.Port)
 	if err != nil {
-		log.Fatalf("Error resolving source address: %s", err)
+		log.Fatal().Err(err).Str("address", opts.Port).Msg("Error resolving source address")
 	}
 
 	targetAddr, err := net.ResolveUDPAddr("udp", opts.Target)
 	if err != nil {
-		log.Fatalf("Error resolving target address: %s", err)
+		log.Fatal().Err(err).Str("address", opts.Target).Msg("Error resolving target address")
 	}
 
 	listenConn, err := net.ListenUDP("udp", sourceAddr)
 	if err != nil {
-		log.Fatalf("Error listening on %s: %s", sourceAddr, err)
+		log.Fatal().Err(err).Str("address", opts.Port).Msg("Error listening on address")
 	}
 	defer listenConn.Close()
 
 	targetConn, err := net.DialUDP("udp", nil, targetAddr)
 	if err != nil {
-		log.Fatalf("Error connecting to %s: %s", targetAddr, err)
+		log.Fatal().Err(err).Str("address", opts.Target).Msg("Error connecting to target")
 	}
 	defer targetConn.Close()
 
-	log.Printf("Listening on %s, forwarding to %s", sourceAddr, targetAddr)
+	log.Info().Str("source", sourceAddr.String()).Str("target", targetAddr.String()).Msgf("Listening on %s, forwarding to %s", sourceAddr, targetAddr)
 
 	for {
 		buffer := make([]byte, opts.Buffer)
 		n, addr, err := listenConn.ReadFromUDP(buffer)
 
 		if err != nil {
-			log.Printf("Error reading from %s: %s", addr, err)
+			log.Warn().Err(err).Msg("Error reading from UDP socket")
 			continue
 		}
 
-		log.Printf("Received %d bytes from %s", n, addr)
+		log.Info().Int("length", n).Str("source", addr.String()).Msg("Received packet")
 		if opts.Dump {
 			if opts.Json {
-				var msg bytes.Buffer
-				err = json.Indent(&msg, buffer[:n], "", "  ")
-				if err != nil {
-					log.Printf("Wrong JSON?\n%s", buffer[:n])
-				} else {
-					log.Printf("\n%s", msg.String())
-				}
+				log.Debug().RawJSON("data", buffer[:n]).Msg("Data (JSON)")
+			} else if opts.Hex {
+				log.Debug().Hex("data", buffer[:n]).Msg("Data (Hex)")
 			} else {
-				log.Printf("Data: %s", strings.TrimSpace(string(buffer[:n])))
+				log.Debug().Str("data", strings.TrimSpace(string(buffer[:n]))).Msg("Data")
 			}
 		}
 
 		_, err = targetConn.Write(buffer[:n])
 		if err != nil {
-			log.Printf("Error writing to %s: %s", targetAddr, err)
+			log.Warn().Err(err).Str("target", targetAddr.String()).Msg("Error writing to target")
 		}
 	}
 }
